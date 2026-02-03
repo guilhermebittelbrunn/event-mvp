6c71dd67919830adf7a8724b72b09bc282f86920
guilhermebittelbrunn <guilherme.bitte@unifebe.edu.br>
2026-02-03 08:42:41 -0300

    refactor: remove StripeWebhookGuard from payment processing controller

--- src/module/webhook/useCases/payment/processPayment/processPayment.controller.ts
+++ src/module/webhook/useCases/payment/processPayment/processPayment.controller.ts
@@ -1,17 +1,16 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { ProcessPaymentDTO } from './dto/processPayment.dto';
 import { ProcessPaymentService } from './processPayment.service';
 
-import { StripeWebhookGuard } from '@/shared/guards/stripeWebhook.guard';
-
 @Controller('/webhook/payment/process-payment')
-@UseGuards(StripeWebhookGuard)
+// @UseGuards(StripeWebhookGuard)
 export class ProcessPaymentController {
   constructor(private readonly useCase: ProcessPaymentService) {}
 
   @Post()
   async handle(@Body() body: ProcessPaymentDTO): Promise<void> {
+    console.log('body', JSON.stringify(body, null, 2));
     /** @todo: validate the webhook signature */
     if (body?.data?.object?.id) {
       await this.useCase.execute(body);
